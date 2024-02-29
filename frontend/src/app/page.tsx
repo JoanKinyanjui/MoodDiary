cd428ad2bfbd7567e7e7c01a99a943772f6f5866
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2024-02-29 10:13:34 +0300

    web3 integration

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,97 @@
 "use client"
 import MainPage from "@/components/MainPage";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import {useEffect, useRef, useState } from "react";
+import Web3Modal from 'web3modal';
+import {ethers} from 'ethers';
+import {abi} from "@/constants/abi";
 
 export default function Home() {
-  const [mood,setMood] = useState<string>('');
-  const [walletConnected,setWalletConnected] = useState<boolean>(false);
-  const [address,setAddress] = useState<string>('')
+  const [mood, setMood] = useState<string>('');
+  const [walletConnected, setWalletConnected] = useState<boolean>(false);
+  const [connecting,setConnecting] = useState<boolean>(false);
+  const [address, setAddress] = useState<string>('');
+  const [transactionStatus,setTransactionStatus] = useState<string>('Pick a mood')
+  const web3ModalRef = useRef<Web3Modal>();
+  const contractAddress:string = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 
-  const onChangeMood = (moodLabel:string) =>{
-    setMood(moodLabel);
+  const getProviderOrSigner = async(needSigner:boolean = false)=>{
+    setConnecting(true);
+  try{
+    const provider = await web3ModalRef.current?.connect();
+    const web3Provider = new ethers.BrowserProvider(provider)
+
+    
+    let {chainId} = await web3Provider.getNetwork();
+    console.log(chainId)
+  
+
+   const signer = await web3Provider.getSigner();
+   const _address  = await signer.getAddress();
+   setAddress(_address);
+
+   if(needSigner){
+    return signer;
+   }
+   return web3Provider;
+
+  }catch{
+
+  }
   }
 
+  const connectWallet = async()=>{
+    console.log('connect clicked')
+  try{
+    getProviderOrSigner();
+    setWalletConnected(true);
+    setConnecting(false);
+  }catch(error){
+    console.log("We couldn't connect :", error)
+  }
+  }
+
+  // Get Mood
+  const getMood = async()=>{
+    try {
+      const providerOrSigner = await getProviderOrSigner(true); 
+      const contract = new ethers.Contract(contractAddress, abi, providerOrSigner);
+      const currentMood = await contract.getMood();
+      setMood(currentMood);
+      console.log("This is the currentMood",currentMood)
+    } catch (error) {
+      console.error('Failed to get mood:', error);
+    }
+  }
+// Set Mood
+  const onChangeMood = async (str:string) =>{
+    try{
+  const providerOrSigner = await getProviderOrSigner(true);
+  const contract = new ethers.Contract(contractAddress,abi,providerOrSigner)
+  console.log(str);
+  const tx = await contract.setMood(str);
+  setTransactionStatus('transaction processing ....');
+  await tx.wait();
+  setMood(str);
+  setTransactionStatus('transaction confirmed,' + str + + " " + "mood updated");
+  console.log("Mood updated to:", str);
+    }catch{
+
+    }
+  }
+  
+
+  useEffect(()=>{
+    web3ModalRef.current = new Web3Modal({
+      network: 'sepolia',
+      providerOptions: {},
+      disableInjectedProvider: false,
+    });
+  },[])
 
 
   return (
-   <div>
-   <MainPage onChangeMood={onChangeMood} mood={mood} address={address} />
-   </div>
+    <div>
+      <MainPage onChangeMood={onChangeMood} mood={mood} address={address}  connectWallet={connectWallet} transactionStatus={transactionStatus} />
+    </div>
   );
 }
--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -5,9 +5,11 @@ type moodDiaryProps ={
   onChangeMood : (mood:string)=>void;
   mood:string;
   address :string;
+  transactionStatus:string;
+  connectWallet :()=>void;
 }
 
-function MainPage({mood , onChangeMood, address}:moodDiaryProps) {
+function MainPage({mood , onChangeMood, address,connectWallet,transactionStatus}:moodDiaryProps) {
   const backgroundColor = mood === 'happy' ? 'bg-yellow-400' :
   mood === 'calm' ? 'bg-[gainsboro]' : 
   mood === 'sad' ? 'bg-blue-950' :
@@ -25,12 +27,13 @@ function MainPage({mood , onChangeMood, address}:moodDiaryProps) {
     <button className={`bg-gray-900 px-4 py-2 rounded-md h-[47px] flex justify-center items-center w-[23%] md:w-[200px] text-[10px]  sm:text-[12px] md:text-[14px]`}  onClick={()=>{onChangeMood('sad')}}>SAD</button>
     <button className={`bg-gray-900 px-4 py-2 rounded-md h-[47px] flex justify-center items-center w-[23%] md:w-[200px] text-[10px]  sm:text-[12px] md:text-[14px]`}  onClick={()=>{onChangeMood('angry')}}>ANGRY</button>
    </div>
+   <p className='mx-auto text-indigo-300 texxt-lg '>{transactionStatus}</p>
   </div>
 
   <div className='absolute top-0 right-0 w-full  h-[50px] md:h-[70px]  bg-white text-black flex items-center  '>
   <div className='w-11/12 md:w-[70%] mx-auto  flex justify-between items-center'>
   <p className='font-semibold text-[18px] sm:text-[20px] md:text-[24px] text-gray-900'>Mood Diary</p>
-    <button className='px-2 py-2 text-white bg-gray-900 flex rounded-md'>{address ? '' :'Connect Wallet'}</button>
+    <button className='px-2 py-2 text-white bg-gray-900 flex rounded-md' onClick={connectWallet}>{address ? address.slice(0,6).concat('...') :'Connect Wallet'}</button>
   </div>
   </div>
 
@@ -38,4 +41,4 @@ function MainPage({mood , onChangeMood, address}:moodDiaryProps) {
   )
 }
 
-export default MainPage
+export default MainPage;
